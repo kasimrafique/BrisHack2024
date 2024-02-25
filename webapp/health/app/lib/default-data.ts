a0ba4d83e5d56975fd82925708c22b689ac96076
Ashby Thorpe <ashbythorpe@gmail.com>
2024-02-25 23:32:28 +0000

    Add england data for other years

--- webapp/health/app/lib/default-data.ts
+++ webapp/health/app/lib/default-data.ts
@@ -11,6 +11,76 @@ export const england_2020 = {
     "road_safety": 102.8
 }
 
+export const england_2019 = {
+    "area_type": "Country",
+    "life_satisfaction": 100.7,
+    "healthy_eating": 95.6,
+    "physical_activity": 102.2,
+    "green_space": 99.9,
+    "gp_distance": 99.4,
+    "pharmacy_distance": 100,
+    "sport_facility_distance": 99.5,
+    "air_pollution": 98.9,
+    "noise_complaints": 100.8,
+    "road_safety": 101.1
+}
+
+export const england_2018 = {
+    "area_type": "Country",
+    "life_satisfaction": 103.5,
+    "healthy_eating": 96.1,
+    "physical_activity": 100.3,
+    "green_space": 99.9,
+    "gp_distance": 99.8,
+    "pharmacy_distance": 100.1,
+    "sport_facility_distance": 99.6,
+    "air_pollution": 99.3,
+    "noise_complaints": 99.4,
+    "road_safety": 100.8
+}
+
+export const england_2017 = {
+    "area_type": "Country",
+    "life_satisfaction": 102,
+    "healthy_eating": 101.2,
+    "physical_activity": 99.7,
+    "green_space": 100,
+    "gp_distance": 99.8,
+    "pharmacy_distance": 100.1,
+    "sport_facility_distance": 99.8,
+    "air_pollution": 100,
+    "noise_complaints": 99.4,
+    "road_safety": 100.4
+}
+
+export const england_2016 = {
+    "area_type": "Country",
+    "life_satisfaction": 101.7,
+    "healthy_eating": 100,
+    "physical_activity": 100,
+    "green_space": 100,
+    "gp_distance": 99.9,
+    "pharmacy_distance": 100,
+    "sport_facility_distance": 100,
+    "air_pollution": 95.5,
+    "noise_complaints": 99.6,
+    "road_safety": 100.1
+}
+
+export const england_2015 = {
+    "area_type": "Country",
+    "life_satisfaction": 100,
+    "healthy_eating": 100,
+    "physical_activity": 100,
+    "green_space": 100,
+    "gp_distance": 100,
+    "pharmacy_distance": 100,
+    "sport_facility_distance": 100,
+    "air_pollution": 100,
+    "noise_complaints": 100,
+    "road_safety": 100
+}
+
 export const standard_deviations = {
     "life_satisfaction": 11.5855,
     "healthy_eating": 10.1449,
@@ -24,7 +94,7 @@ export const standard_deviations = {
     "road_safety": 9.8203
 };
 
-export const Keys : string[] = [
+export const Keys: string[] = [
     "Life Satisfaction",
     "Healthy Eating",
     "Physical Activity",
@@ -32,4 +102,4 @@ export const Keys : string[] = [
     "Air Pollution",
     "Noise Complaints",
     "Road Safety"
-  ]
+]
