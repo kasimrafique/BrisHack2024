8b259868cc71e84a17dd67f2665aa248ba333fe0
Kasim Rafique <kasim@rafique.co.uk>
2024-02-26 09:02:44 +0000

    ui

--- webapp/health/app/page.tsx
+++ webapp/health/app/page.tsx
@@ -14,30 +14,46 @@ export default function Home() {
     const [state, dispatch] = useFormState(getPostcode, {});
 
     return (
-        <div className="bg-white x h-screen fill-white ">
+        <><div className="bg-white x h-screen fill-white ">
             <div className={`flex justify-center w-screen object-fill bg-dark-green text-5xl text-white  py-20 ${montserrat.className}`}>
                 <p>How Healthy is your Postcode?</p>
             </div>
 
 
-            <div className="flex justify-center grid-cols-3 mr-5">
-                <div className="flex pl-2 pr-1 pt-8 pb-5 justify-left text-xl text-dark-green mx-4 my-2"> Enter your postcode here:</div>
-                <form action={dispatch}>
-                    <div className="flex items-center">
-                    <div className="mt-5">
-                    <input type="text" name="postcode" className="mx-8 my-2 p-[5%] text-black rounded-[7px] border border-dark-green" placeholder="  Enter postcode" />
-                    </div>
-                    <div>
-                        <button type="submit" className="text-black justify-center mt-6"><ArrowRightCircleIcon className="h-10 w-10 text-dark-green hover:text-green-800" /></button>
-                    </div>
-                    </div>
-                </form>
-                {
-                    state.error && <div className="flex justify-left text-sm text-red">{state.error}</div>
-                }
-                <div className="flex justify-left text-sm text-light-green"></div>
+            <div className="flex flex-col items-center">
+                <div className="flex justify-center grid-cols-3 mr-5">
+                    <div className="flex pl-2 pr-1 pt-8 pb-5 justify-left text-xl text-dark-green mx-4 my-2"> Enter your postcode here:</div>
+                    <form action={dispatch}>
+                        <div className="flex items-center">
+                            <div className="mt-5">
+                                <input type="text" name="postcode" className="mx-8 my-2 p-[5%] text-black rounded-[7px] border border-dark-green" placeholder="  Enter postcode" />
+                            </div>
+                            <div>
+                                <button type="submit" className="text-black justify-center mt-6"><ArrowRightCircleIcon className="h-10 w-10 text-dark-green hover:text-green-800" /></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                {state.error && <div className="flex justify-left text-sm text-red-300">{state.error}</div>}
             </div>
 
-        </div>
+        </div><footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+                <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                    <span className="text-sm text-light-green text-center "><i>Valid for England Postcodes only</i></span>
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 Pancakes</span>
+                    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                        <li>
+                            <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                        </li>
+                
+                        <li>
+                            <a href="mailto:askusanything@ashbythorpe.com?Subject=HAemailbounced" className="hover:underline">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </footer></>
+
+
+
     );
 }
--- webapp/health/app/postcode/[postcode]/input.tsx
+++ webapp/health/app/postcode/[postcode]/input.tsx
@@ -3,6 +3,7 @@
 import { getPostcode } from "@/app/lib/actions";
 import { useFormState } from "react-dom";
 import { Montserrat } from "next/font/google";
+import { ArrowRightCircleIcon } from "@heroicons/react/24/outline";
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 
@@ -11,10 +12,20 @@ export default function Input(){
 
     return (
         
-        <form action={dispatch}>
-            <i className={`flex justify-left text-2xl font-semi-bold text-white mx-8 pt-3 ${montserrat.className}`}>Find out about another area:</i>
-            <input type="text" name="postcode" className="mx-8 my-2 text-black rounded-[7px] border border-dark-green" placeholder="  Enter postcode" />
-            <button type="submit" className="text-black">Submit</button>
-        </form>
+        
+            <>
+                <form action={dispatch}>
+                    <i className={`flex justify-left text-2xl font-semi-bold text-white mx-8 pt-3 ${montserrat.className}`}>Find out about another area:</i>
+                    <input type="text" name="postcode" className="mx-8 my-2 py-5 text-black border border-dark-green rounded-xl" placeholder="  Enter postcode" />
+                    <button type="submit" className="text-black"><ArrowRightCircleIcon className="h-12 w-12 mz-3 text-green-300 hover:text-green-900" /></button>
+
+                </form>
+                {
+                    state.error && <div className="flex justify-around text-sm text-red">{state.error}</div>
+                }
+                {/* <div className="flex justify-left text-sm text-light-green"></div> */}
+                        {/* <button type="submit" className="text-black justify-center"><ArrowRightCircleIcon className="h-10 w-10 text-dark-green hover:text-green-800" /></button> */}
+            </>
+
     );
 }
