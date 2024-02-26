57b486994221f3d39eb076af060ebeefff903092
Ashby Thorpe <ashbythorpe@gmail.com>
2024-02-26 09:42:42 +0000

    Fix plots

---
+++ webapp/health/app/lib/utils.ts
@@ -0,0 +1,32 @@
+import { GraphData } from "./graphs";
+
+// Doesn't actually transpose
+export function transpose(data: GraphData[] | null) {
+    let data_t: (number | null)[][] = [];
+    if (!data) {
+        return data_t;
+    }
+
+    for (const x of data) {
+        let inner: (number | null)[] = [];
+        for (const key_t in x) {
+            const key = key_t as keyof GraphData;
+            inner.push(x[key]);
+        }
+        data_t.push(inner);
+    }
+
+    return data_t;
+}
+
+export function actually_transpose<T>(data: T[][]): T[][] {
+    let data_t: T[][] = [];
+    for (let i = 0; i < data[0].length; i++) {
+        let inner: T[] = [];
+        for (let j = 0; j < data.length; j++) {
+            inner.push(data[j][i]);
+        }
+        data_t.push(inner);
+    }
+    return data_t;
+}
